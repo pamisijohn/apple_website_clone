f3c8ebb01827d4b7fc11612cd8fa6d215731a24a
John Pamisi <pamisijohn@gmail.com>
2024-07-03 11:59:27 +0100

    nav and hero section

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,15 @@
+const Highlights = () => {
+  return (
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc">
+      <div>
+        <h1 id="title" className="section-heading">
+          Get the highlights
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default Highlights;
